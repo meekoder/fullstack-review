0c2ecb3048d2588402c4c65ea72e182340e795b8
meekoder <meekorusdi@gmail.com>
2020-11-06 13:23:30 -0800

    successful get request from github api

--- database/index.js
+++ database/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const github = require('../helpers/github');
 mongoose
   .set('useNewUrlParser', true)
   .set('useUnifiedTopology', true)
@@ -22,11 +23,13 @@ let repoSchema = mongoose.Schema(
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (body) => {
+  console.log('body', body)
   // This function should save a repo or repos to the MongoDB
   const repo = new Repo(body);
+  console.log('repo', repo)
   repo.save()
-    .then(res => console.log(res))
-    .catch(e => console.log(e));
+    .then(() => github.getReposByUsername(body.owner.login))
+    .catch(e => console.log('error', e));
 }
 
 module.exports.save = save;
--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,23 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    method: 'GET',
+    url: `https://api.github.com/users/${username}`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+  axios(options)
+    .then(res => console.log(res))
+    .catch(e => console.log(e));
 
 }
 
