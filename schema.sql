ac003d9a2cbc35ff3def02fcc84d91a0d83bfe81
meekoder <meekorusdi@gmail.com>
2020-11-07 16:41:24 -0800

    changed schema and only adding entries to db if they dont exist already

--- schema.sql
+++ schema.sql
@@ -4,13 +4,12 @@ CREATE DATABASE repos;
 USE repos;
 
 CREATE TABLE repos (
-  id INT NOT NULL AUTO_INCREMENT,
-  repoid BIGINT,
+  repoId BIGINT,
   repoName VARCHAR(255),
   repoUrl VARCHAR(255),
   stars BIGINT,
   username VARCHAR(255),
   avatarUrl VARCHAR(255),
   userUrl VARCHAR(255),
-  PRIMARY KEY (id)
+  PRIMARY KEY (repoId)
 );
